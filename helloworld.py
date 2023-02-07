43d2b22ec6d27058af7314409618845537765af1
nur bujang <>
2023-02-07 17:17:57 +0000

    week 1 task

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,12 @@
+# helloworld.py
+# Author: Nur Bujang
+# Weekly Task 1
+# other task is to create a Github account with mywork and pands-problem-sheet repositories
+# to create, commit and push a python program that displays Hello World! when it is run
+
+# print () function is to create an output onto the screen
+# it can be a string or other object which will be converted into a string before getting printed on the screen
+# both single or double quotes can be used, but double quotes are generally used for string representation
+# single quotes are generally used for regular expressions. Be consistent.
+# round brackets are generally used for function
 print("Hello World!")
