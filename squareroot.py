596dc58066d05e9c21b5954c23164e988ff5e0fc
nur bujang <>
2023-03-05 14:30:25 +0000

    week 6 task

--- squareroot.py
+++ squareroot.py
@@ -12,14 +12,14 @@
 # The square root of 14.5 is approx. 3.8.
 
 # the Newton Method square root approximation formula
-# newtsqrt = 0.5*(guess+(flonum/guess)), let num be any number, and guess is the approximation of the square root of num
+# newtsqrt = 0.5*(guess+(flonum/guess)), let flonum be any number, and guess is the approximation of the square root of flonum
 
 flonum=float(input("Please enter a positive number: ")) # prompt to input a positive float number
 
-def newtsqrt(flonum): # define the function newtsqrt
-    guess=0.5*flonum # to get the square root of flonum ONLY
-    better=0.5*(guess+flonum/guess)  # to calculate a better approximation of guess
-    while better!=guess: # while loop, as long as better is not equal to approx
+def newtsqrt(flonum): # define the function newtsqrt with 1 argument flonum
+    guess=0.5*flonum # variable declaration to get the square root of flonum ONLY
+    better=0.5*(guess+flonum/guess)  # variable declaration to calculate a better approximation of guess
+    while better!=guess: # while loop, as long as better is not equal to guess
         guess=better # this is to hold the value
         better=0.5*(guess+flonum/guess)  # updating the guess
     return round(better,1) # return number and round up to 1 decimal point
