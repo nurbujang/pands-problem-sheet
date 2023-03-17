5aa2c2e7f4132c5992233b0b393e3233d3f21caf
nur bujang <>
2023-03-17 15:10:16 +0000

    week 8 task

--- plottask.py
+++ plottask.py
@@ -7,3 +7,38 @@
 # and a plot of the function  h(x)=x3 in the range [0, 10], 
 # on the one set of axes.
 # add legends etc to the plot
+
+# import modules
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import matplotlib.lines as mlines
+
+# this is so that the "random" numbers are the same each time
+np.random.seed(1)
+#create normal distribution curve
+normData=np.random.normal(loc = 5, scale = 2, size = 1000)
+plt.hist(normData, color='royalblue')
+
+# create data
+x = np.array(range(1, 10))
+# multiply each entry by itself twice to get y=x*x*x
+y = x*x*x 
+
+# customize plot
+plt.plot(x, y, marker = '*', ms = 10, mec = 'm', ls = '-.', linewidth = '2', color ='#B8860B', label = "$x^3$")
+# declare font and font size
+font1 = {'size':15}
+font2 = {'size':13.5}
+# customize plot title and axis labels, x$^3$=$x^3$ for superscript
+plt.title("Figure 1: h(x)=$x^3$ Plot", color ='#191970', fontdict = font1, fontweight='bold')
+plt.xlabel("x", color ='#00008B', fontdict = font2, style='oblique', fontweight='bold')
+plt.ylabel("y", color ='#00008B', fontdict = font2, style='oblique', fontweight='bold', rotation = 0)
+# customize legend
+royalblue_patch = mpatches.Patch(color='royalblue', label='random values')
+magenta_line = mlines.Line2D([], [], mec='magenta', marker='*', markersize=10, ls = '-.', linewidth = '2', color ='#B8860B')
+
+plt.legend([royalblue_patch, magenta_line], ["random values", "$x^3$"], loc='upper left')
+plt.grid(axis = 'y', color = '#8FBC8F', linestyle = '--', linewidth = 0.3)
+# show plot
+plt.show()
