25f42ef0430809fd05bbfec216e2fda8cb55192f
nur bujang <>
2023-03-17 15:44:51 +0000

    week 8 task

--- plottask.py
+++ plottask.py
@@ -28,8 +28,8 @@ y = x*x*x
 # customize plot
 plt.plot(x, y, marker = '*', ms = 10, mec = 'm', ls = '-.', linewidth = '2', color ='#B8860B', label = "$x^3$")
 # declare font and font size
-font1 = {'size':15}
-font2 = {'size':13.5}
+font1 = {'family':'fantasy', 'size':17}
+font2 = {'family':'sans-serif', 'size':14}
 # customize plot title and axis labels, x$^3$=$x^3$ for superscript
 plt.title("Figure 1: h(x)=$x^3$ Plot", color ='#191970', fontdict = font1, fontweight='bold')
 plt.xlabel("x", color ='#00008B', fontdict = font2, style='oblique', fontweight='bold')
@@ -37,7 +37,6 @@ plt.ylabel("y", color ='#00008B', fontdict = font2, style='oblique', fontweight=
 # customize legend
 royalblue_patch = mpatches.Patch(color='royalblue', label='random values')
 magenta_line = mlines.Line2D([], [], mec='magenta', marker='*', markersize=10, ls = '-.', linewidth = '2', color ='#B8860B')
-
 plt.legend([royalblue_patch, magenta_line], ["random values", "$x^3$"], loc='upper left')
 plt.grid(axis = 'y', color = '#8FBC8F', linestyle = '--', linewidth = 0.3)
 # show plot
